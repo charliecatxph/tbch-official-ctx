d341fa989efa1bcf4ed94b540c8a53c361e08e6c
Charl Concepcion <76478646+charliecatxph@users.noreply.github.com>
2022-07-15 20:55:06 +0800

    server fix

--- backend/index.js
+++ backend/index.js
@@ -234,6 +234,7 @@ mongoose.connect(process.env.DB).then((d) => {
 
   
 }).catch(e => {
+    console.log(e)
     app.get("*", (req, res) => {
         res.send("Can't connect to server. Please contact @charliecatxph about this issue.");
     })
